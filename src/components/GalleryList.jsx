393a0ecb08b092760057f1e1af5b7f782dfc1d6d
MANISH KUMAR <113700493+Manish202276@users.noreply.github.com>
2024-02-05 20:13:37 +0530

    Update GalleryList.jsx

--- src/components/GalleryList.jsx
+++ src/components/GalleryList.jsx
@@ -5,25 +5,32 @@ export default function Gallerylist()
 {   
     const [Galleryimage,setGalleryimage]=useState([]);
     const [isLoading,setisLoading]=useState(true);
-    async function download_gallery(){
-        const response=await axios.get('https://api.slingacademy.com/v1/sample-data/photos');
+    const [offvalue,setoffvalue]=useState(0);
+    const [url,seturl]=useState(`https://api.slingacademy.com/v1/sample-data/photos?offset=${offvalue}&limit=20`);
+        async function download_gallery(){
+        const response=await axios.get(url);
         const responsedata=response.data;
         const responsedataimg=responsedata.photos;
         const res=responsedataimg.map((galle)=>{
             return{
                 photos:galle.url
+                
             }
         })   
     setGalleryimage(res);
     setisLoading(false);
     }
-    useEffect(()=>{download_gallery()},[])
+    useEffect(()=>{download_gallery()},[url])
     return(
         <>
-            <div className="flex flex-row  h-full w-full">
+            <div className="box">
                 {(isLoading)?'Loading....':
                 Galleryimage.map((p)=><Gallery image={p.photos} />)
                 }      
+            </div>
+            <div className='next'>
+                <button onClick={()=>seturl(`https://api.slingacademy.com/v1/sample-data/photos?offset=${offvalue=offvalue-20}&limit=20`)} className='previous-button'>Previous</button>
+                <button onClick={()=>seturl(`https://api.slingacademy.com/v1/sample-data/photos?offset=${offvalue=offvalue+20}&limit=20`)} className='next-button'>Next</button>
             </div>
         </>
     )
