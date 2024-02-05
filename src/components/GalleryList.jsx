45af5b983e7092699cbafc2da45893a02fbe6f3a
Rohitgut <rohit1322001@gmail.com>
2024-02-05 15:09:55 +0530

    Add next or prev functions ,check kr le acche se ek baar.

--- src/components/GalleryList.jsx
+++ src/components/GalleryList.jsx
@@ -5,8 +5,11 @@ export default function Gallerylist()
 {   
     const [Galleryimage,setGalleryimage]=useState([]);
     const [isLoading,setisLoading]=useState(true);
+    const [offset,setoffset]=useState(0);
+
     async function download_gallery(){
-        const response=await axios.get('https://api.slingacademy.com/v1/sample-data/photos?limit=20');
+        
+        const response=await axios.get('https://api.slingacademy.com/v1/sample-data/photos?limit=20&offset='+offset);
         const responsedata=response.data;
         const responsedataimg=responsedata.photos;
         const res=responsedataimg.map((galle)=>{
@@ -14,16 +17,42 @@ export default function Gallerylist()
                 photos:galle.url
             }
         })   
+
     setGalleryimage(res);
     setisLoading(false);
     }
-    useEffect(()=>{download_gallery()},[])
+
+    const next=()=>{
+        if(offset>132){
+        alert("next photos...");
+        }
+        else{
+        setoffset(offset+20);
+        }
+        // console.log(offset);
+        
+    }
+    const prev=()=>{
+        if(offset<0){
+        alert("prev photos...");
+        }
+        else{
+        setoffset(offset-20);
+        }
+        // console.log(offset);
+    }
+
+    useEffect(()=>{download_gallery()})
     return(
         <>
             <div className="box">
                 {(isLoading)?'Loading....':
                 Galleryimage.map((p)=><Gallery image={p.photos} />)
                 }      
+            </div>
+            <div>
+                <button onClick={prev}>Previous</button>
+                <button onClick={next}>Next</button>
             </div>
         </>
     )
