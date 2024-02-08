3102345190aff416a414a9fcf92fee6139a42028
Rohitgut <rohit1322001@gmail.com>
2024-02-08 23:13:24 +0530

    Done.....

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,5 @@
 import GalleryList from './GalleryList';
-export default function Gallery({image},{title}){
+export default function Gallery({image,title}){
     return(
         <div className='box2'>
         <h4>{title}</h4>
